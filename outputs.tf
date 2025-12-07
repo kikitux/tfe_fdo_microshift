2c3af49b41bd9d8bcae7e6db863c14ba57a03dbb
kikitux <kikitux@gmail.com>
2025-12-07 09:52:42 +0000

    update output for minio api and console

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "postgres_port_forward_command" {
   value = "kubectl port-forward -n ${var.namespace} svc/${kubernetes_service.postgres.metadata[0].name} ${kubernetes_service.postgres.spec[0].port[0].port}:${kubernetes_service.postgres.spec[0].port[0].port}"
 }
 
-output "minio_port_forward_command" {
+output "minio_port_forward_command_api" {
+  value = "kubectl port-forward -n ${var.namespace} svc/${kubernetes_service.minio.metadata[0].name} ${kubernetes_service.minio.spec[0].port[0].port}:${kubernetes_service.minio.spec[0].port[0].port}"
+}
+
+output "minio_port_forward_command_console" {
   value = "kubectl port-forward -n ${var.namespace} svc/${kubernetes_service.minio.metadata[0].name} ${kubernetes_service.minio.spec[0].port[1].port}:${kubernetes_service.minio.spec[0].port[1].port}"
 }
+
